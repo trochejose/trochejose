48d553512b7c31e9e417ce8d88c2316783821e27
jose troche <trochejose6@gmail.com>
2016-09-28 22:26:43 +0000

    Added association for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927003109) do
+ActiveRecord::Schema.define(version: 20160928222348) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
